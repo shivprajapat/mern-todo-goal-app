77bcd1ca1b36863ada37d5d3d554d48504b25f67
shivprajapat <shivkumarprajapat232@gmail.com>
2023-02-18 17:23:03 +0530

    Add Dummy route

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+router.get("/", (req, res) => {
+    res.status(200).json({ message: "hello goals!" })
+})
+router.post("/", (req, res) => {
+    res.status(200).json({ message: "Post goals!" })
+})
+router.put("/:id", (req, res) => {
+    res.status(200).json({ message: `Update goal ${req.params.id}` })
+})
+router.delete("/:id", (req, res) => {
+    res.status(200).json({ message: `Delete goal ${req.params.id}` })
+})
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,5 @@ const port = process.env.PORT || 5000
 
 const app = express()
 
-app.get("/api/goals",(req,res)=>{
-res.json({message :"hello goals!"})
-})
+app.use('/api/goals', require("./routes/goalRoutes"))
 app.listen(port, () => console.log(`Server Magic on port  ✅ : ${port}`))
