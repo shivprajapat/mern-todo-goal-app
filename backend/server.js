a0728a837225505169f056ba90892ebdb17deeb4
shivprajapat <shivkumarprajapat232@gmail.com>
2023-02-18 17:02:58 +0530

    goals route create

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const dotenv = require('dotenv').config()
+
 const port = process.env.PORT || 5000
 
 const app = express()
+
+app.get("/api/goals",(req,res)=>{
+res.json({message :"hello goals!"})
+})
 app.listen(port, () => console.log(`Server Magic on port  ✅ : ${port}`))
