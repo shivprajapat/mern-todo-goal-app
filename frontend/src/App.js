c574d793812811f8482d2b9ff63f347321e44e92
shivprajapat <shivkumarprajapat232@gmail.com>
2023-02-21 16:26:59 +0530

    header, login and register page Create

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Container } from 'react-bootstrap'
+import { Route, Routes } from 'react-router-dom'
+import { Header } from './components'
+import { Dashboard, Login, Register } from './pages'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Fragment>
+    <Header />
+    <Container className='mt-4'>
+      <Routes>
+        <Route path="/" element={<Dashboard/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+      </Routes>
+    </Container>
+  </Fragment>
   )
 }
 
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Link, NavLink } from 'react-router-dom'
+import { FaSignInAlt, FaUser } from 'react-icons/fa'
+
+const Header = () => {
+    return (
+        <Navbar bg="primary" variant="dark">
+            <Container>
+                <Navbar.Brand to="/" as={Link}>
+                    GoalSetter
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link as={NavLink} to="/login"><FaSignInAlt /> <span>Login</span></Nav.Link>
+                        <Nav.Link as={NavLink} to="/register"><FaUser /> <span>Register</span></Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Header
